fc1c19fb25fcb43748691db44bf273ab76adce36
Misha Causur <mishacausur@mail.ru>
2024-09-23 23:47:42 +0500

    int like var

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Integer x = scanner.nextInt();
-        Integer o = scanner.nextInt();
-        Integer result = x + o;
-        System.out.println(result);
+        int x = 5;
+        x = 3;
+        System.out.println(x);
     }
 }
