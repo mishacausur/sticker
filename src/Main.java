741b09ac2de23e1522987bbbdbd0dc4d360fa665
Misha Causur <mishacausur@mail.ru>
2024-09-27 19:37:13 +0500

    loops added w/ exception

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,13 @@ import en.services.CustomService;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         int count = 10;
 
         while (count > 0) {
             System.out.println("counter is in progress");
+            Thread.sleep(300);
         }
     }
 }
