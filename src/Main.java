f5faea02890fb315a8ddfe554608eb45cc1d37a4
Misha Causur <mishacausur@mail.ru>
2024-10-05 20:47:34 +0500

    task 2/1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,38 @@
 import java.util.Scanner;
-import en.services.CustomService;
 
 public class Main {
+    public static void main(String[] args) {
 
-    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
 
+        int counter = 0;
+        while(true) {
+            System.out.println("Введите год");
+            int enteredYear = scanner.nextInt();
+            System.out.println("Введите количество дней:");
+            int enteredDays = scanner.nextInt();
+            int days = calculateDays(enteredYear);
 
-        for (int i = 0; i < 10; i++) {
-            System.out.println("counter is in progress, " + i);
-            Thread.sleep(300);
+            if (enteredDays == days) {
+                counter++;
+            }
+            if (enteredDays != days) {
+                System.out.println("Неправильно! В этом году " + days + " дней!");
+                System.out.println("Набрано очков: " + counter);
+                break;
+            }
+        }
+    }
+
+    public static int calculateDays(int year) {
+        if (year % 400 == 0) {
+            return 366;
+        } else if (year % 100 == 0) {
+            return 365;
+        } else if (year % 4 == 0) {
+            return 366;
+        } else {
+            return 365;
         }
     }
 }
