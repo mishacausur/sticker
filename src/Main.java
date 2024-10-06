dfc807e4a5ce26a09d4b50a6bf103affba72bf95
Misha Causur <mishacausur@mail.ru>
2024-10-06 21:38:13 +0500

    task 3/1

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,75 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        String text = """
+                Выберите операцию и введите её номер:
+                1. Добавить новый доход
+                2. Добавить новый расход
+                3. Выбрать систему налогообложения""";
+
         Scanner scanner = new Scanner(System.in);
 
-        int counter = 0;
-        while(true) {
-            System.out.println("Введите год");
-            int enteredYear = scanner.nextInt();
-            System.out.println("Введите количество дней:");
-            int enteredDays = scanner.nextInt();
-            int days = calculateDays(enteredYear);
+        int earnings = 0;
+        int spendings = 0;
 
-            if (enteredDays == days) {
-                counter++;
-            }
-            if (enteredDays != days) {
-                System.out.println("Неправильно! В этом году " + days + " дней!");
-                System.out.println("Набрано очков: " + counter);
+        while (true) {
+            System.out.println(text);
+            String input = scanner.nextLine();
+            if ("end".equals(input)) {
                 break;
             }
+            int operation = Integer.parseInt(input);
+            switch (operation) {
+                case 1:
+                    System.out.println("Введите сумму дохода:");
+                    String earingMoneyStr = scanner.nextLine();
+                    int earingMoney = Integer.parseInt(earingMoneyStr);
+                    earnings += earingMoney;
+                    break;
+                case 2:
+                    System.out.println("Введите сумму расхода:");
+                    String spendingMoneyStr = scanner.nextLine();
+                    int spendingMoney = Integer.parseInt(spendingMoneyStr);
+                    spendings += spendingMoney;
+                    break;
+                case 3:
+                    int justTax = taxEarnings(earnings);
+                    int taxWithSpendings = taxEarningsMinusSpendings(earnings, spendings);
+                    System.out.println(compareTaxes(justTax, taxWithSpendings));
+                    break;
+                default:
+                    System.out.println("Такой операции нет");
+            }
         }
+        System.out.println("Программа завершена!");
     }
 
-    public static int calculateDays(int year) {
-        if (year % 400 == 0) {
-            return 366;
-        } else if (year % 100 == 0) {
-            return 365;
-        } else if (year % 4 == 0) {
-            return 366;
+    public static int taxEarnings(int earnings) {
+        return earnings * 6 / 100;
+    }
+
+    public static int taxEarningsMinusSpendings(int earnings, int spendings) {
+        int tax = (earnings - spendings) * 15 / 100;
+        return tax >= 0 ? tax : 0;
+    }
+
+    public static String compareTaxes(int justTax, int taxWithSpendings) {
+        if (justTax < taxWithSpendings) {
+            String text = """
+                    Мы советуем вам УСН доходы
+                    Ваш налог составит: %d рублей
+                    Налог на другой системе: %d рублей
+                    Экономия: %d рублей
+                    """;
+            return String.format(text, justTax, taxWithSpendings, (taxWithSpendings - justTax));
         } else {
-            return 365;
+            String text = """
+                    Мы советуем вам УСН доходы минус расходы
+                    Ваш налог составит: %d рублей
+                    Налог на другой системе: %d рублей
+                    Экономия: %d рублей
+                    """;
+            return String.format(text, taxWithSpendings, justTax, (justTax - taxWithSpendings));
         }
     }
 }
