c065e4a87584d01cf48e025c502406802a7d7424
Misha Causur <mishacausur@mail.ru>
2024-10-29 21:25:49 +0500

    poly

--- src/Main.java
+++ src/Main.java
@@ -1,77 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        SmartRobot robot = new SmartRobot();
-        robot.moveDown();
-        robot.moveDown();
-        robot.moveLeft();
-        robot.moveUp();
-        robot.moveDown();
-        robot.moveLeft();
-        robot.moveLeft();
-
-        System.out.println("Координаты робота: " + robot.getX() + ":" + robot.getY());
-        System.out.println("Количество шагов: " + robot.getStepsCount());
+        A a = new B();
+        System.out.println(a.get());
     }
 }
 
-class Robot {
-    private int x;
-    private int y;
-
-    public void moveRight() {
-        x++;
-    }
-
-    public void moveLeft() {
-        x--;
-    }
-
-    public void moveUp() {
-        y--;
-    }
-
-    public void moveDown() {
-        y++;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
+class A {
+    public char get() {
+        return 'A';
     }
 }
 
-class SmartRobot extends Robot {
-    private int count = 0;
-
+class B extends A {
     @Override
-    public void moveRight() {
-        super.moveRight();
-        count++;
-    }
-
-    @Override
-    public void moveLeft() {
-        super.moveLeft();
-        count++;
-    }
-
-    @Override
-    public void moveUp() {
-        super.moveUp();
-        count++;
-    }
-
-    @Override
-    public void moveDown() {
-        super.moveDown();
-        count++;
-    }
-
-    public int getStepsCount() {
-        return count;
+    public char get() {
+        return 'B';
     }
 }
