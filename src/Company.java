0e45d2d4c8e5c892c06b4147e82d939a4abc41c5
Misha Causur <mishacausur@mail.ru>
2024-10-30 21:30:14 +0500

    poly got started

---
+++ src/Company.java
@@ -0,0 +1,12 @@
+import taxes.TaxSystem;
+
+public class Company {
+    protected String title;
+    private int debit;
+    private int credit;
+    private TaxSystem taxSystem;
+
+    public void shiftMoney(int amount) {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        A a = new B();
-        System.out.println(a.get());
+
     }
 }
 
-class A {
-    public char get() {
-        return 'A';
-    }
-}
-
-class B extends A {
-    @Override
-    public char get() {
-        return 'B';
-    }
-}
