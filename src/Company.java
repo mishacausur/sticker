0f2e85e658f10c74f75cf1aad7a3a4812d65d834
Misha Causur <mishacausur@mail.ru>
2024-10-31 21:39:28 +0500

    company class done

--- src/Company.java
+++ src/Company.java
@@ -2,11 +2,32 @@ import taxes.TaxSystem;
 
 public class Company {
     protected String title;
-    private int debit;
-    private int credit;
+    private int debit = 0;
+    private int credit = 0;
     private TaxSystem taxSystem;
 
-    public void shiftMoney(int amount) {
+    public Company(String title, TaxSystem taxSystem) {
+        this.title = title;
+        this.taxSystem = taxSystem;
+    }
 
+    public void shiftMoney(int amount) {
+       if (amount > 0) {
+           debit += amount;
+       } else if (amount < 0) {
+           credit += Math.abs(amount);
+       }
+    }
+
+    public void setTaxSystem(TaxSystem taxSystem) {
+        this.taxSystem = taxSystem;
+    }
+
+    public void payTaxes() {
+        int tax = 0;
+        System.out.printf("Компания %s уплатила налог в размере: %d руб.", title, tax);
+        System.out.println();
+        debit = 0;
+        credit = 0;
     }
 }
