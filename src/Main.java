37a5a7c0e143f21d4f9e17c2cfb8c447d352f60e
Misha Causur <mishacausur@mail.ru>
2024-11-09 23:20:41 +0500

    password checker is done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
+import PasswordChecker.PasswordChecker;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        boolean isWorking = true;
+        Scanner scanner = new Scanner(System.in);
+        PasswordChecker checker = new PasswordChecker();
 
+        System.out.println("Введите мин. длину пароля: ");
+        int minLenght = scanner.nextInt();
+        try {
+            checker.setMinLength(minLenght);
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+        System.out.println("Введите макс. допустимое количество повторений символа подряд: ");
+        int maxRepeats = scanner.nextInt();
+        try {
+            checker.setMaxRepeats(maxRepeats);
+        } catch (Exception e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+
+        while(isWorking) {
+            System.out.println("Введите пароль или end: ");
+            String input = scanner.next();
+            if (input.equals("end")) {
+                isWorking = false;
+            }
+            try {
+                if (checker.verify(input)) {
+                    System.out.println("Подходит!");
+                } else {
+                    System.out.println("Не подходит!");
+                }
+            } catch (Exception e) {
+                System.out.println(e);;
+                System.exit(1);
+            }
+        }
     }
 }
 
