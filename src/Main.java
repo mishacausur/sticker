c12377f0ef40c615f82b1d056e3b946dc2377a04
Misha Causur <mishacausur@mail.ru>
2024-11-26 20:13:51 +0500

    done tree hm

--- src/Main.java
+++ src/Main.java
@@ -51,10 +51,34 @@ class Person {
 }
 
 
+//Критерии отбора таковы. При сравнении двух кандидатов лучший выбирается по принципу:
+//
+//тот у кого больше опыта; если опыт одинаковый то
+//тот, у кого в имени с фамилией (т.е. name) больше букв s или S (от слова space - космос; было решено, что медийность - важная часть космонавтики); если по этому критерию оказываются равны, то
+//тот, у кого имя с фамилией (т.е. name) короче (так легче будет запоминать имена астронавтов по всему миру)
+
 class SpacePersonComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person o1, Person o2) {
-        return 0;
+        if (o1.getExperience() != o2.getExperience()) {
+            return Integer.compare(o2.getExperience(), o1.getExperience());
+        }
+        int sCount = Integer.compare(countS(o2.getName()), countS(o1.getName()));
+        if (sCount != 0) {
+            return sCount;
+        } else {
+            return Integer.compare(o1.getName().length(), o2.getName().length());
+        }
+    }
+
+    private int countS(String name) {
+        int count = 0;
+        for (Character c: name.toLowerCase().toCharArray()) {
+            if ('c' == c) {
+                count++;
+            }
+        }
+        return count;
     }
 }
